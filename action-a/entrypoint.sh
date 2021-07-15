f8eb81d222aca8ed0de8fd21acb17ba861e7f70a
Yangyanyu9 <84946727+Yangyanyu9@users.noreply.github.com>
2021-07-15 10:41:07 +0800

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,1 +1,17 @@
 ENTRYPOINT ["/entrypoint.sh"]
+#sum1=0
+import FJSP as NSGA-II
+k=1000
+for i in range(1):
+    import datetime
+    starttime = datetime.datetime.now()
+    model=NSGA_II(200,100,200)
+    model.main()
+    endtime = datetime.datetime.now()
+    print(model.min_sum)
+    print (endtime - starttime)
+    if k>int(model.min_sum):
+        k=int(model.min_sum)
+        l=model.t_mac
+    
+k
